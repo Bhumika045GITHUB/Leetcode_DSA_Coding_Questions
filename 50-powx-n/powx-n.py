3d7975a9edbb5b996735b0e8b962cf1d1840b1e3
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-01-13 20:08:20 +0530

    Time: 42 ms (35.20%) | Memory: 17.5 MB (5.04%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,17 @@
 class Solution:
+    @cache
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        #tc - O(n)
+     #   return x**n
+        #tc - O(logn) - by divide n conquer approach
+        if(n==0):
+            return 1
+        elif(n==1):
+            return x
+        elif(n<0):
+            return self.myPow(1/x,-n)
+        elif(n%2==0):
+            return self.myPow(x,n/2) * self.myPow(x,n/2)
+        else:
+            return x*self.myPow(x,n-1)
+
