27c10783acdab5b4ae64f501e8ac93562d59104d
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-01-13 18:41:40 +0530

    Time: 38 ms (69.73%) | Memory: 17.4 MB (24.88%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        #inplace
+        #inplace - splitted s.split() string to list ------> then ----> reversed list then joind iwth spave in string
+        
         return (" ".join(s.split()[::-1]))
      
 
