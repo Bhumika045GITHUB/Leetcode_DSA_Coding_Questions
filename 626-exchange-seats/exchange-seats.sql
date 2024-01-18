672f6ac07e394d4c77d37bf4beccd23b36f271f6
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-01-18 16:03:53 +0530

    Time: 558 ms (92.23%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,2 @@
-# Write your MySQL query statement below
 
-with cte as(
-select *, lead(student) over(order by id) as next_student, lag(student) over(order by id) as prev_student 
-from seat)
-
-select id, 
-case when (id%2 = 0) then prev_student else coalesce(next_student,student) end as student
-from cte;
+select if(id<(select max(id) from seat),if(id%2=0,id-1,id+1),if(id%2=0,id-1,id)) as id, student from seat order by id;
