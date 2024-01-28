a33df055379af32077181788f4cdafae2485fe8d
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-01-28 11:04:53 +0530

    Time: 1324 ms (39.89%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,15 +1,10 @@
-# Write your MySQL query statement below
-SELECT 
-    Department,
-    Employee,
-    Salary
-FROM (SELECT 
-    d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary,
-    DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
-FROM Employee e
-JOIN Department d
-ON e.departmentId = d.id) AS t
-WHERE rnk = 1
-order by salary desc;
+/* Write your PL/SQL query statement below */
+SELECT DEPT.name AS Department, EMP.name AS Employee, EMP.salary AS 
+
+Salary FROM Department DEPT, Employee EMP WHERE
+
+EMP.departmentId = DEPT.id AND (EMP.departmentId, salary) IN 
+
+(SELECT departmentId, MAX(salary) FROM Employee GROUP BY 
+
+departmentId)
