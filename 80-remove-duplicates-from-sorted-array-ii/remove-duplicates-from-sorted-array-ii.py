956ad8b1c1ad7ba65a72076cbd73c7bdcbe41d25
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-03-13 00:26:05 +0530

    Time: 50 ms (69.68%) | Memory: 16.6 MB (80.73%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(len(nums)-2,0,-1):
             if(nums[i]==nums[i-1] and nums[i]==nums[i+1]):
                 nums.pop(i-1)
+                #nums.pop(i+1)
         return len(nums)
 
 
