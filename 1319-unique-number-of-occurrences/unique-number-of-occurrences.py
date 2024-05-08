98f7d876968364dd41054f466a88cd4f98587873
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-05-08 23:54:55 +0530

    Time: 24 ms (30.94%) | Memory: 11.5 MB (97.84%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def uniqueOccurrences(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: bool
-        """
-        print(Counter(arr).values())
+        
+        #print(Counter(arr).values())
         return len(set(Counter(arr).values())) == len(Counter(arr).values())
 
 
