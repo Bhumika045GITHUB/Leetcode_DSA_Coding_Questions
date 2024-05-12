77d20e4d084d7717e1a8e8432853a7b016695671
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-05-12 23:58:38 +0530

    Time: 17 ms (78.49%) | Memory: 12.1 MB (16.58%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        return sum(i < j for i, j in enumerate(sorted(citations, reverse=True)))
+        
