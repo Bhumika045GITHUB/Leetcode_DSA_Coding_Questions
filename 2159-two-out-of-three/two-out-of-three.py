c34350611ae0d9b91f439680d8971826880afcd3
Bhumika Lalchandani <63667709+Bhumika045GITHUB@users.noreply.github.com>
2024-05-31 16:37:01 +0530

    Time: 39 ms (93.79%) | Memory: 11.7 MB (51.98%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.py
+++ 2159-two-out-of-three/two-out-of-three.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def twoOutOfThree(self, nums1, nums2, nums3):
-        return set(nums1) & set(nums2) | set(nums2) & set(nums3) | set(nums1) & set(nums3)
+        return (list(set(nums1) & set(nums2) | set(nums2) & set(nums3) | set(nums1) & set(nums3)))
         
